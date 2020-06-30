3553374a13aaae56e047f7329480044a540de0a3
Boufaa <boufaah@gmail.com>
2020-06-30 13:52:11 +0100

    Projet_Final

--- Plugin_form/Plugin_form.php
+++ Plugin_form/Plugin_form.php
@@ -8,43 +8,187 @@ Author URI: https://automattic.com/wordpressBrief3/
 License: GPLv2 or later
 Text Domain: Plugin_form
 */
-?>
+
+ 
 
 
 
+ 
+
+add_action("admin_menu","addMenu");
+
+ 
+
+
+function addMenu() {
+    add_menu_page( 'Plugin_form', 'Plugin_form', 'read', 'Plugin_form_Dashboard', 'Plugin_form_index' );
+    add_submenu_page( 'Plugin_form_Dashboard', 'Plugin_form', 'Settings', 'read', 'Plugin_form_Dashboard', 'Plugin_form_index');
+    add_submenu_page( 'Plugin_form_Dashboard', 'Plugin_form', 'infos', 'read', 'Plugin_form_NouvellePage', 'Plugin_form_NouvellePage');
+}
+
+ 
+
+ 
+
+function Plugin_form_index(){
+  require_once( dirname( dirname( dirname( dirname( __FILE__ )))) . '/wp-load.php' );
+  global $wpdb;
+  if(isset($_POST['submit'])){
+      $option=$_POST['option'];
+      $name = $_POST['inputtxt'];
+      $email = $_POST['inputarea'];
+$table_name = $wpdb->prefix . "plugin_table";
+
+ 
+
+$wpdb->insert( $table_name, array(
+                      'inputtxt' => $name,
+                      'inputarea' => $email,
+                      'option'=>$option
+                      )); 
+  echo ' 
+    <div id="message" class="updated below-h2">
+      <p>data registred check your info page</p>
+    </div>
+  ';
+  }
+?>     
+<h1 style="font-size:23px">Register some infos</h1> 
+ <form action="" method="post">
+  <table style="background-color: #fff; width: 100%;">
+      <tr>
+        <td><label style="font-size:20px" for="inputtxt">Utilisateur :</label></td>
+        <td><input style="width:400px" type="text" name="inputtxt" id="inputtxt"  required/><br></td>
+      </tr>
+      <tr>
+        <td><label style="font-size:20px" for="inputarea">Description :</label></td>
+        <td><textarea style="width:400px" type="text" name="inputarea" id="inputarea"  required></textarea></td>
+      </tr>
+      <tr>
+        <td><label style="font-size:20px" for="option">Options :</label></td>
+        <td>
+          <select style="width:400px" name="option" id="option" required>
+            <option value="">--Select--</option>
+            <option value="A">Option A</option>
+            <option value="B">Option B</option>
+            <option value="C">Option C</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+      <br><br><td colspan="2" ><br><input type="submit" class="button button-primary button-hero load-customize hide-if-no-customize" name="submit"/></td>
+      </tr>
+    </table>
+</form>
 <?php
 
-add_action('admin_menu', 'my_admin_menu');
+ 
 
-function my_admin_menu () {
+}
+
+ 
+
+
+function Plugin_form_NouvellePage(){
     
-	 //parameters details
-	 //add_management_page($page_title, $menu_title, $capability,$menu_slug, $function);
     
-	 //add a new setting page udner setting menu
-    // add_management_page('Description', 'Description', 'manage_options',__FILE__,'Description_admin_page');
-     //add new menu and its sub menu 
-    add_menu_page('Description title', 'Plugin_form', 'manage_options','Description_page', 'Description_admin_page');
-    add_submenu_page( 'Description_page', 'Page title', 'Settings','manage_options', 'Settings', 'mt_settings_page');
+    echo '<h1 style="font-size:23px ">All Data</h1>';
+    
+    
+    global $wpdb;
+    $table_name = $wpdb->prefix . "plugin_table";
+    $results = $wpdb->get_results( "SELECT * FROM $table_name"); 
+    ?>
    
+    <?php
+if(!empty($results))  
+{  
     
+    
+    
+    
+    ?>  
+    
+    <table style="width:70%" class="wp-list-table widefat fixed striped pages text-center" >
+        <thead  >
+            <tr >
+                <th>Utilisateur</th>
+                <th>Description</th>
+                <th>Options</th>
+            </tr>
+        </thead>
+        <tbody  id="the-list">
+    <?php
+          
+    foreach($results as $row){   
+        
+        ?>
+        <tr>
+            <td><?php echo $row->inputtxt ;?></td>
+            <td><?php echo $row->inputarea ;?></td>
+            <td><?php echo $row->option ;?></td>
+        </tr>
+     
+        <?php
+    }
+    ?>
+    <tbody>
+  </table>
+<?php
+}else{
+    echo 'there is no data for now';
+}
+?>
+    
+<?php
+}
+//runs when plugin is activated
+register_activation_hook( __FILE__, 'create_table' );
+
+ 
+
+function create_table(){
+    global $wpdb;
+
+ 
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+ 
+
+    $table_name = $wpdb->prefix . "plugin_table";  
+
+ 
+
+    $sql = "CREATE TABLE $table_name (
+      id int(10) unsigned NOT NULL AUTO_INCREMENT,
+      inputtxt varchar(255) NOT NULL,
+      inputarea varchar(255) NOT NULL,
+      option varchar(5) NOT NULL,
+      
+      PRIMARY KEY  (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+ 
+
+    dbDelta( $sql );
 }
 
+ 
 
-function Description_admin_page () {
-  echo '<div class="wrap">
-  <h1>Hello!</h1>
-  <p>it is a plugin with two submenus. A page for the general description of my plugin.Configuration page an input text field, textarea for the description,
-     an option list and a save button
-  </p>
-</div>';
-include_once('affichage.php');
-}
+// Runs when plugin is desactivated
+register_deactivation_hook( __FILE__, 'remove_table' );
 
-// mt_settings_page() displays the page content for the Test Settings submenu
-function mt_settings_page() {
-    echo "<h2>" . __( 'Settings Configurations', 'menu-test' ) . "</h2>";
-	include_once('setting_page.php');
-}
+ 
+
+function remove_table() {
+    global $wpdb;
+     $table_name = $wpdb->prefix . 'plugin_table';
+     $sql = "DROP TABLE IF EXISTS $table_name";
+     $wpdb->query($sql);
+     delete_option("my_plugin_db_version");
+} 
+
+ 
 
 ?>
